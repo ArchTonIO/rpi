f0188e646bebf3227fae1d174c49bac55782a10a
Antonio Del Cogliano <delchi1199@gmail.com>
2021-04-05 16:03:37 +0200

    Added drawAnimation()

--- 1306/pico-sdk/oled.h
+++ 1306/pico-sdk/oled.h
@@ -9,5 +9,9 @@ void ssd1306_print(const char* str);
 void draw_letter_at(uint8_t x, uint8_t y, char c);
 void drawBitmap(int16_t x, int16_t y, const uint8_t bitmap[], int16_t w,
 		int16_t h, uint16_t color);
+
+void drawAnimation(int16_t x, int16_t y, const uint8_t bitmaps_array[][1024], int16_t w,
+		int16_t h, uint16_t color, uint8_t frames_number, uint16_t duration_ms);
+
 void show_scr();
 void fill_scr(uint8_t v);
